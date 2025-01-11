cdae5f57ed66742b767fdfc1459bd756e87f6d80
Aman raj <48181289+amaanraaz@users.noreply.github.com>
2025-01-11 13:43:28 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   region = "us-east-1"
+  version = "~> 4.0" 
 }
 
 terraform {
+  required_version = ">= 1.0.0"
   backend "s3" {
     bucket = "nginx-bucket-backend"
     key    = "terraform.tfstate"
