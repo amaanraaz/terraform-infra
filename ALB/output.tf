a36449651e3cb7f3d5e758e0cfdeb063fa414c48
Aman raj <48181289+amaanraaz@users.noreply.github.com>
2025-01-11 21:18:35 +0530

    Update output.tf of alb

--- ALB/output.tf
+++ ALB/output.tf
@@ -1,9 +1,8 @@
-output "aws_lb" {
-  description = "ID of the public instance"
-  value       = aws_lb.tf-lb
-}
-
 output "aws_lb_tg" {
   description = "aws lb tg"
   value = aws_lb_target_group.my_target_group.arn
+}
+output "alb_dns_name" {
+    description = "dns address"
+    value = aws_lb.tf-lb.dns_name
 }
